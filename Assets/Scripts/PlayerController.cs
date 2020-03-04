3f51a6e2981e7c8543e4942f8199e3fb8d741e5e
vrp56 <38020844+vrp56@users.noreply.github.com>
2020-03-04 17:17:51 -0600

    Implemented a basic animation controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+
+    CharacterController characterController;
+
+
+    // Movement Variables
+    public float speed = 6.0f;
+    public float gravity = 20.0f;
+    private Vector3 moveDirection = Vector3.zero;
+
+    [SerializeField]
+    private float _movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("Run", true);
+            if (characterController.isGrounded)
+            {
+                // We are grounded, so recalculate
+                // move direction directly from axes
+
+                moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+                moveDirection *= speed;
+            }
+            // Move the controller
+            characterController.Move(moveDirection * Time.deltaTime);
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("Run", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("Attack");
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+
+
+    }
+}
