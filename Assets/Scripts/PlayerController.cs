a79ce749d32b62d024908ab756cd83a03f115091
vrp56 <38020844+vrp56@users.noreply.github.com>
2020-03-04 21:59:51 -0600

    Added movement to character and have a camera that follows the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,51 +10,63 @@ public class PlayerController : MonoBehaviour
 
 
     // Movement Variables
-    public float speed = 6.0f;
+    public float speed = 10.0f;
+    public float rotSpeed = 100.0f;
     public float gravity = 20.0f;
     private Vector3 moveDirection = Vector3.zero;
 
-    [SerializeField]
-    private float _movementSpeed;
+   
+    
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    void MoveCharacter()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (characterController.isGrounded)
         {
-            animator.SetBool("Run", true);
-            if (characterController.isGrounded)
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
             {
-                // We are grounded, so recalculate
-                // move direction directly from axes
-
-                moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-                moveDirection *= speed;
+                animator.SetBool("Run", true);
+                animator.SetBool("Attack", false);
             }
-            // Move the controller
-            characterController.Move(moveDirection * Time.deltaTime);
-        }
-        else if (Input.GetKeyUp(KeyCode.W))
-        {
-            animator.SetBool("Run", false);
-        }
+            else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+            {
+                animator.SetBool("Run", false);
+            }
 
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            animator.SetTrigger("Attack");
+            if (Input.GetKeyDown(KeyCode.F) && !Input.GetKeyDown(KeyCode.W))
+            {
+                animator.SetBool("Attack", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.F))
+            {
+                animator.SetBool("Attack", false);
+            }
         }
 
         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
         // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
         // as an acceleration (ms^-2)
         moveDirection.y -= gravity * Time.deltaTime;
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+        }
 
-
-
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCharacter();
+        
     }
 }
